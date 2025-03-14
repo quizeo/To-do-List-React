adc7c0d5db1db19422ecb38b6ee9946453519fb8
John Lloyd Quizeo <johnkiezquizeo@gmail.com>
2025-03-15 03:02:00 +0800

    Updates

---
+++ precache-manifest.8c9837a46e09a964188954a69830c8d5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7285f93a433ca6ce217ed9b5b7100404",
+    "url": "/To-do-List-React/index.html"
+  },
+  {
+    "revision": "fc28a69f039cdff4147f",
+    "url": "/To-do-List-React/static/js/2.0672a4aa.chunk.js"
+  },
+  {
+    "revision": "dac33d0b7614f3a83a87",
+    "url": "/To-do-List-React/static/js/main.05e0f1fc.chunk.js"
+  },
+  {
+    "revision": "8fe117302d5cb4183e50",
+    "url": "/To-do-List-React/static/js/runtime-main.d818d34a.js"
+  }
+]);
